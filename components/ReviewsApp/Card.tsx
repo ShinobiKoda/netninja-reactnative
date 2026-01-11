6c92e4a346b6ab01cabfa74df7ad4e02a4e41ee1
ShinobiKoda <adebiyipacsc2022@futa.edu.ng>
2026-01-11 18:11:11 +0100

    refactor: simplify Card component by using destructured props and improving formatting

--- components/ReviewsApp/Card.tsx
+++ components/ReviewsApp/Card.tsx
@@ -1,34 +1,29 @@
-import {Text, View, StyleSheet} from "react-native"
+import { ReactNode } from "react";
+import { StyleSheet, View } from "react-native";
 
-
-
-
-export default function Card(props){
-  return(
+export default function Card({ children }: { children?: ReactNode }) {
+  return (
     <View style={styles.card}>
-      <View style={styles.cardContent}>
-        {props.children}
-      </View>
+      <View style={styles.cardContent}>{children}</View>
     </View>
-  )
+  );
 }
 
-const styles= StyleSheet.create({
+const styles = StyleSheet.create({
   card: {
     borderRadius: 6,
     elevation: 3,
     backgroundColor: "#fff",
-    shadowOffset: {width: 1, height: 1},
+    shadowOffset: { width: 1, height: 1 },
     shadowColor: "#333",
     shadowOpacity: 0.3,
     shadowRadius: 2,
     marginHorizontal: 4,
-    marginVertical: 6
+    marginVertical: 6,
   },
 
   cardContent: {
     marginHorizontal: 18,
     marginVertical: 20,
-  }
-})
-
+  },
+});
